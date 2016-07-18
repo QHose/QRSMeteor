16ff5e049cd1834a0c315fb83e6c9c0ccfeda330
QHose <mbj@qlik.com>
2016-07-18 20:51:26 +0200

    google

--- server/main.js
+++ server/main.js
@@ -247,3 +247,13 @@ Meteor.methods({
 // },
 
 
+// first, remove configuration entry in case service is already configured
+ServiceConfiguration.configurations.remove({
+  service: "google"
+});
+ServiceConfiguration.configurations.insert({
+  service: "google",
+  clientId: "411682799732-r38hgfr9nlh0c0udigt4ln23ma8db93k.apps.googleusercontent.com",
+  loginStyle: "popup",
+  secret: "YCP43OPc_-tGlVqvj-o-hbu1"
+});
