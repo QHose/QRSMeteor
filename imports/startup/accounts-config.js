bdf90ae2d0c805b5b9a1a17503fe7792f127ddca
QHose <mbj@qlik.com>
2016-07-25 20:47:18 +0200

    alles werkt met sign up

--- imports/startup/accounts-config.js
+++ imports/startup/accounts-config.js
@@ -1,89 +1,62 @@
-import { Accounts } from 'meteor/accounts-base';
+// import { Accounts } from 'meteor/accounts-base';
  
-Accounts.ui.config({
-  passwordSignupFields: 'USERNAME_ONLY',
+AccountsTemplates.configure({
+    // Behavior
+    confirmPassword: false,
+    enablePasswordChange: true,
+    forbidClientAccountCreation: false,
+    overrideLoginErrors: true,
+    sendVerificationEmail: false,
+    lowercaseUsername: false,
+    focusFirstInput: true,
+
+    // Appearance
+    showAddRemoveServices: false,
+    showForgotPasswordLink: false,
+    showLabels: true,
+    showPlaceholders: true,
+    showResendVerificationEmailLink: false,
+
+    // Client-side Validation
+    continuousValidation: false,
+    negativeFeedback: false,
+    negativeValidation: true,
+    positiveValidation: true,
+    positiveFeedback: true,
+    showValidating: true,
+
+    // // Privacy Policy and Terms of Use
+    // privacyUrl: 'privacy',
+    // termsUrl: 'terms-of-use',
+
+    // Redirects
+    homeRoutePath: '/',
+    redirectTimeout: 4000,
+
+    // // Hooks
+    // onLogoutHook: myLogoutFunc,
+    // onSubmitHook: mySubmitFunc,
+    // preSignUpHook: myPreSubmitFunc,
+    // postSignUpHook: myPostSubmitFunc,
+
+    // Texts
+    texts: {
+      button: {
+          signUp: "Register Now!"
+      },
+      socialSignUp: "Register",
+      socialIcons: {
+          "meteor-developer": "fa fa-rocket"
+      },
+      title: {
+          forgotPwd: "Recover Your Password"
+      },
+    },
 });
 
-
-// AccountsTemplates.configure({
-//     // Behavior
-//     confirmPassword: false,
-//     enablePasswordChange: true,
-//     forbidClientAccountCreation: false,
-//     overrideLoginErrors: true,
-//     sendVerificationEmail: false,
-//     lowercaseUsername: false,
-//     focusFirstInput: true,
-
-//     // Appearance
-//     showAddRemoveServices: false,
-//     showForgotPasswordLink: false,
-//     showLabels: true,
-//     showPlaceholders: true,
-//     showResendVerificationEmailLink: false,
-
-//     // Client-side Validation
-//     continuousValidation: false,
-//     negativeFeedback: false,
-//     negativeValidation: true,
-//     positiveValidation: true,
-//     positiveFeedback: true,
-//     showValidating: true,
-
-//     // Privacy Policy and Terms of Use
-//     privacyUrl: 'privacy',
-//     termsUrl: 'terms-of-use',
-
-//     // Redirects
-//     homeRoutePath: '/',
-//     redirectTimeout: 4000,
-
-//     // // Hooks
-//     // onLogoutHook: myLogoutFunc,
-//     // onSubmitHook: mySubmitFunc,
-//     // preSignUpHook: myPreSubmitFunc,
-//     // postSignUpHook: myPostSubmitFunc,
-
-//     // Texts
-//     texts: {
-//       button: {
-//           signUp: "Register Now!"
-//       },
-//       socialSignUp: "Register",
-//       socialIcons: {
-//           "meteor-developer": "fa fa-rocket"
-//       },
-//       title: {
-//           forgotPwd: "Recover Your Password"
-//       },
-//     },
-// });
-
-
-// // AccountsTemplates.configure({
-// //     defaultLayout: 'layout',
-// // });
-
-// AccountsTemplates.configureRoute('signIn');
-// AccountsTemplates.configureRoute('changePwd');
-// AccountsTemplates.configureRoute('enrollAccount');
-// // AccountsTemplates.configureRoute('forgotPwd');
-// AccountsTemplates.configureRoute('resetPwd');
-// AccountsTemplates.configureRoute('signUp');
-
-
-// // Accounts.ui.config({
-// //     passwordSignupFields: 'USERNAME_ONLY',
-// // });
-
-
-
-// // AdminConfig = {
-// //     collections: {
-// //         users: {},
-// //         apps: {},
-// //         streams: {},
-// //         generatedResources: {}
-// //     }
-// // }
-
+AccountsTemplates.configureRoute('signIn');
+AccountsTemplates.configureRoute('changePwd');
+AccountsTemplates.configureRoute('enrollAccount');
+// AccountsTemplates.configureRoute('forgotPwd');
+AccountsTemplates.configureRoute('resetPwd');
+AccountsTemplates.configureRoute('signUp');
--- imports/ui/security_templates.js
+++ imports/ui/security_templates.js
@@ -13,7 +13,31 @@ Template.register.events({
         Accounts.createUser({
             email: email,
             password: password
+        }, function(error) {
+            if (error) {
+                console.log(error.reason); // Output error if registration fails
+            } else {
+                Router.go("home"); // Redirect user if registration succeeds
+            }
         });
-     Router.go('generation');
+        Router.go('generation');
     }
 });
+
+
+Template.login.events({
+    'submit form': function(event) {
+        event.preventDefault();
+        var email = $('[name=email]')
+            .val();
+        var password = $('[name=password]')
+            .val();
+        Meteor.loginWithPassword(email, password, function(error) {
+            if (error) {
+                console.log(error.reason);
+            } else {
+                Router.go("home");
+            }
+        });
+    }
+})
