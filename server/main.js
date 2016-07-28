16579c0ddfd32dc2109c6477b5e6e92113a58cfa
QHose <mbj@qlik.com>
2016-07-28 19:52:07 +0200

    root url

--- server/main.js
+++ server/main.js
@@ -24,6 +24,7 @@ var qsocks = require('qsocks');
 Meteor.startup(function() {
 
     process.env.ROOT_URL = 'http://' + Meteor.settings.public.host;
+    console.log('********* Meteor runs on host ROOT_URL: ', process.env.ROOT_URL);
 
     console.log('********* On meteor startup, Meteor tool registers itself at Qlik Sense to get notifications from Sense on changes to apps and streams.');
     console.log('********* we try to register a notification on this URL: HTTP post to http://' + senseConfig.SenseServerInternalLanIP +':' + senseConfig.port + '/' + senseConfig.virtualProxy + '/qrs/notification?name=app');
