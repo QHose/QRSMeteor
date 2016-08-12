3af5acfe70157a23edc723d61de05e54193d1cd0
QHose <mbj@qlik.com>
2016-08-12 08:22:01 +0200

    New step by step video and modals, extra nav bar items

--- imports/ui/pages/introduction.js
+++ imports/ui/pages/introduction.js
@@ -4,10 +4,21 @@ import '/imports/ui/UIHelpers';
 import { APILogs } from '/imports/api/APILogs';
 
 
+
+
+Template.introduction.events({
+    'click .stepByStep' () {
+        $('.ui.modal.stepByStep')
+            .modal('show');
+    }
+});
+
 Template.introduction.onRendered(function() {
-    console.log('introduction onRendered');
+    // console.log('introduction onRendered');
 
-     Template.instance().$('.ui.embed').embed();
+    Template.instance()
+        .$('.ui.embed')
+        .embed();
 
-  
+
 })
