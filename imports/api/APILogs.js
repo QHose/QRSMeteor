582e11c8603a915f4b4c4357db0be32d7cfcedf8
QHose <mbj@qlik.com>
2016-09-23 22:02:34 +0200

    fix raw collection client

--- imports/api/APILogs.js
+++ imports/api/APILogs.js
@@ -1,10 +1,16 @@
 import { Mongo } from 'meteor/mongo';
- 
-export const APILogs = new Mongo.Collection('apiLogs'); 
 
-export function REST_Log(call, userId='Not defined'){
-	call.createDate = new Date();
-	call.createdBy = userId
-	APILogs.rawCollection().insert(call);
-	return;
+export const APILogs = new Mongo.Collection('apiLogs');
+
+export function REST_Log(call, userId = 'Not defined') {
+    call.createDate = new Date();
+    call.createdBy = userId
+    if (Meteor.isServer) {
+        APILogs.rawCollection().insert(call);
+    } else {
+        APILogs.insert(call);
+
+    }
+    return;
+
 }
