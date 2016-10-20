abbc44c1ccc660647f9662b06a6e3698c66ade90
QHose <mbj@qlik.com>
2016-10-20 15:44:23 +0200

    fluid accordion

--- imports/ui/generation.js
+++ imports/ui/generation.js
@@ -271,7 +271,8 @@ Template.generation.onCreated(function() {
 Template.generationStreams.onRendered(function() {
     this.$('.title')
         .popup({
-            content: 'Via the QRS API we obtained the list of streams',
+            title: 'Streams',
+            content: 'Via the QRS API we obtained the list of streams. Streams are a way to group apps that share a common purpose. In our case we give each customer its own stream.',
             delay: {
                 show: 2500,
                 hide: 0
@@ -282,7 +283,8 @@ Template.generationStreams.onRendered(function() {
 Template.generationApps.onRendered(function() {
     this.$('.title')
         .popup({
-            content: 'Via the QRS API we obtained the list of streams',
+            title: 'Apps',
+            content: 'Via the QRS API we obtained the list of apps (dashboards). The app is at the core of Qlik Sense. An app is loaded with data, and the visualizations the app contains allow you and your colleagues to explore the data.',
             delay: {
                 show: 2500,
                 hide: 0
