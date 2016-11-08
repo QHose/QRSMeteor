a68f24f28a856869de50327734440f921d67f1a0
QHose <mbj@qlik.com>
2016-11-08 14:40:14 +0100

    new architecture pictures and text

--- imports/ui/steps.js
+++ imports/ui/steps.js
@@ -27,3 +27,17 @@ Template.steps.helpers({
         return Session.get('generated?') ? 'completed' : 'active';
     },
 });
+
+
+Template.steps.onRendered(function() {
+    this.$('.step')
+        .popup({
+            title: 'Demo steps',
+            content: 'This demo simulates the SaaS platform admin console of an OEM partner. In order to finish the demo, you must complete the following steps. If you just started we already selected some dummy customers and a an app. In step 3 we will give each of your customers a private copy of this app. In step 4 you can test Qlik Sense embedded with some users.',
+            delay: {
+                show: 500,
+                hide: 0
+            }
+        });
+})
+
