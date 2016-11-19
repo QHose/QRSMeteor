67750f8676b3a963100e2ed922ad345c47d66241
QHose <mbj@qlik.com>
2016-11-19 11:27:14 +0100

    removed facebook, set min ssbi card height

--- client/main.js
+++ client/main.js
@@ -58,22 +58,22 @@ Meteor.startup(function() {
     AutoForm.setDefaultTemplate("semanticUI");  
 });
 
-//Facebook sync
- window.fbAsyncInit = function() {
-    FB.init({
-      appId      : Meteor.settings.public.facebook.clientId,
-      xfbml      : true,
-      version    : 'v2.7'
-    });
-  };
+// //Facebook sync
+//  window.fbAsyncInit = function() {
+//     FB.init({
+//       appId      : Meteor.settings.public.facebook.clientId,
+//       xfbml      : true,
+//       version    : 'v2.7'
+//     });
+//   };
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+//   (function(d, s, id){
+//      var js, fjs = d.getElementsByTagName(s)[0];
+//      if (d.getElementById(id)) {return;}
+//      js = d.createElement(s); js.id = id;
+//      js.src = "//connect.facebook.net/en_US/sdk.js";
+//      fjs.parentNode.insertBefore(js, fjs);
+//    }(document, 'script', 'facebook-jssdk'));
 
 //google analytics
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
