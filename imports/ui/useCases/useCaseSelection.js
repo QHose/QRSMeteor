13d9874889add34ab542b0350bb0d717115f16d9
QHose <mbj@qlik.com>
2016-11-24 11:38:32 +0100

    New web integration use case

--- imports/ui/useCases/useCaseSelection.js
+++ imports/ui/useCases/useCaseSelection.js
@@ -4,7 +4,9 @@ import './SSBI/SSBI.js';
 
 
 Template.useCaseSelection.events({
-
+ 'click .webIntegrationDemo' () {
+        login('John');
+    },
 })
 
 
@@ -16,3 +18,21 @@ Template.useCaseSelection.onRendered(function() {
     this.$('.cards')
         .transition('scale in');
 })
+
+function login(user) {
+    console.log('login ', user, Meteor.userId());
+    try {
+
+        Meteor.call('simulateUserLogin', user, (error, result) => {
+            if (error) {
+                sAlert.error(error);
+                console.log(error);
+            } else {
+                console.log('All other users logged out, and we inserted the new user ' + user + ' in the local database');
+                sAlert.success(user + ' is now logged in into Qlik Sense');
+            }
+        })
+    } catch (err) {
+        sAlert.error(err.message);
+    }
+};
