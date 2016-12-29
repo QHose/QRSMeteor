4f8bf9d45f9ec509d6ebaf6571b0e21f0678b093
QHose <mbj@qlik.com>
2016-12-29 14:40:41 +0100

    slide per main chapter works

--- imports/ui/impress/integrationTopicsCircular.js
+++ imports/ui/impress/integrationTopicsCircular.js
@@ -1,4 +1,4 @@
-import { senseConfig, engineConfig, certs, authHeaders } from '/imports/api/config.js';
+import { senseConfig } from '/imports/api/config.js';
 var qsocks = require('qsocks');
 import './integrationTopics.js';
 import lodash from 'lodash';
@@ -8,29 +8,37 @@ const enigma = require('enigma');
 var appId = 'f094b3f0-529f-4c4d-9a60-a1305c8c19b0';
 
 Template.integrationTopicsCircular.helpers({
-   topics() {
-        console.log('integrationTopicsCircular helper get topics');
+    mainTopics() {
+        return Session.get('mainTopics');
+    },
+    topics() {
         return Session.get('integrationTopics');
     },
     level: function(level) {
-      var row = this;
+        var row = this;
         console.log('row is ', this);
         level -= 1;
         return row[level].qText
     },
     allItemsOfLevel: function(level) {
-      console.log('all items of level helper');
+        console.log('all items of level helper');
         var topics = Session.get(integrationTopics);
     },
     XValue(index) {
-      console.log('xvalue helper, x:', index);
+        console.log('xvalue helper, x:', index);
         return 1200 * index;
     }
 });
 
+
 Template.integrationTopicsCircular.onRendered(function() {
-  api = impress();
-        api.init();
+    getTableWithEnigma(appId);
+    getLevel1And2(appId);
+})
+
+Template.integrationTopicsCircular.onRendered(function() {
+    api = impress();
+    api.init();
     // impressInitialized = Session.get('impressInitialized');
     // if (!impressInitialized) {
     //     console.log('impress was NOT yet initialized');
@@ -45,11 +53,6 @@ Template.integrationTopicsCircular.onRendered(function() {
     Template.instance()
         .$('.ui.embed')
         .embed();
-
-
-    getTableWithEnigma(appId);
-    // exampleAlex();
-    // qSocksGetTable('371e1116-02ef-4030-be06-13e8342b6340');
 })
 
 function getValuesOfLevel(level2Text) {
@@ -78,7 +81,7 @@ function getValuesOfLevel(level2Text) {
                                 qMeasures: [{
                                     qDef: {
                                         // qLabel: 'Number of Beers',
-                                        qDef: "=Count({<[Level 2]={'"+level2Text+"'}>}1)"
+                                        qDef: "=Count({<[Level 2]={'" + level2Text + "'}>}1)"
                                     }
                                 }]
                             }
@@ -145,6 +148,55 @@ function getTableWithEnigma(appId) {
 
 }
 
+function getLevel1And2(appId) {
+
+    $.get('https://unpkg.com/enigma.js/schemas/qix/3.1/schema.json')
+        .then(qixschema => {
+
+            enigma.getService('qix', {
+                    schema: qixschema,
+                    appId: appId,
+                    session: { //https://github.com/qlik-oss/enigma.js/blob/master/docs/qix/configuration.md#example-using-nodejs
+                        host: senseConfig.host,
+                        prefix: senseConfig.virtualProxyClientUsage,
+                        port: senseConfig.port,
+                        unsecure: true
+                    }
+                })
+                .then(qix => {
+
+                    qix.app.createSessionObject({
+                            qInfo: { qType: 'cube' },
+                            qHyperCubeDef: {
+                                qDimensions: [{
+                                    qDef: { qFieldDefs: ['Level 1'] }
+                                }, {
+                                    qDef: { qFieldDefs: ['Level 2'] }
+                                }],
+                                qMeasures: [{
+                                    qDef: {
+                                        qLabel: 'Count of level 1',
+                                        qDef: '=Count(1)'
+                                    }
+                                }],
+                            }
+                        })
+                        .then(model => {
+                            model.getHyperCubeData('/qHyperCubeDef', [{ qTop: 0, qLeft: 0, qWidth: 3, qHeight: 3333 }]).then(data => {
+                                console.log('Result set from Qlik Sense:', data);
+                                var table = data[0].qMatrix;
+                                console.log('Main levels contained in QMatrix', table);
+                                Session.set('mainTopics', table)
+                            })
+                        })
+
+                })
+
+        })
+
+
+}
+
 function exampleAlex() {
     $.get('https://unpkg.com/enigma.js/schemas/qix/3.1/schema.json')
         .then(qixschema => {
@@ -184,31 +236,6 @@ function exampleAlex() {
 }
 
 //http://playground.qlik.com/learn/engine/hypercube
-function qSocksGetTable(appId) {
-    var config = {
-        host: senseConfig.host,
-        port: senseConfig.port,
-        isSecure: false,
-        origin: 'localhost'
-    };
-
-    qsocks.Connect(config)
-        .then(function(global) {
-            console.log('connected to Qsocks');
-            _global = global;
-            return global.openDoc(appId, '', '', '', true) //global.openDoc(appId), this code opens the app without data, that is faster!
-        })
-        .then(function(app) {
-            console.log('** QSocks opened the app and now tries to get a hypercube from app: ', appId);
-            // createList(app);
-            createCube(app);
-        })
-        .catch((error) => {
-            console.error('error while getting the table from the app: ', error);
-            throw new Meteor.error(error);
-        });
-}
-
 //https://gist.github.com/mindspank/58b3080a342b1cd937e6
 //https://help.qlik.com/en-US/sense-developer/3.1/Subsystems/EngineAPI/Content/WorkingWithAppsAndVisualizations/CreateVisualizations/create-hypercube.htm
 function createCube(app) {
