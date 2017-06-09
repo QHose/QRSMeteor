b39fa4639811083522ab6860393926d2802e64fc
QHose <mbj@qlik.com>
2017-06-09 15:20:29 +0200

    fix

--- imports/ui/impress/slideSorter.js
+++ imports/ui/impress/slideSorter.js
@@ -1,5 +1,4 @@
 import { Template } from 'meteor/templating';
-import { Session } from 'meteor/session';
 import { senseConfig as config } from '/imports/api/config';
 
 import './impress.css'; //slides you see when you start the multi tenant demo
@@ -12,13 +11,23 @@ import { initializePresentation } from './ppt_integration'
 Template.ppt_slideSorter.onRendered(function() {
     initializePresentation();
     //after the page is loaded, ensure the slides have proper margins, and remove the scrollbar
-    Meteor.setTimeout(function() {
-        this.$('.step')
-            .attr('style', 'margin-top: 40px; max-height: 100%;')
-        this.$('.slideContent').css({ "visibility": "visible" });
-    }, 1000);
+    init();
 })
 
+Template.slideSorter.onRendered(function() {
+    initializePresentation();
+    //after the page is loaded, ensure the slides have proper margins, and remove the scrollbar
+    init();
+})
+
+function init() {
+    Meteor.setTimeout(function() {
+        this.$('.step')
+            .removeClass("integration")
+            .attr('style', 'margin-top: 40px; max-height: 100%;');
+        this.$('.slideContent').css({ "visibility": "visible" });
+    }, 100);
+}
 // Template.slideSorter.onDestroyed(function() {
 //     $('body').attr('style', 'height: 100%;');
 // })
