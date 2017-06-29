47e93f558fea76af876baed47c43ff3dd22adcfd
QHose <mbj@qlik.com>
2017-06-29 14:34:33 +0200

    added rest endpoints for testing,

---
+++ server/restEndpoints.js
@@ -0,0 +1,75 @@
+//https://atmospherejs.com/simple/json-routes
+
+JsonRoutes.add("get", "/OneCustomerWithUsers", function(req, res, next) {
+    // var id = req.params.id;
+    var sampleData = customer1
+
+    JsonRoutes.sendResult(res, {
+        data: customer1;
+    });
+});
+
+JsonRoutes.add("get", "/multipleCustomersWithUsers", function(req, res, next) {
+    // var id = req.params.id;
+    var sampleData = [customer1, customer2]
+
+    JsonRoutes.sendResult(res, {
+        data: [customer1, customer2];
+    });
+});
+
+var customer1 = {
+    "_id": "EXpapRzZXc52B3joK",
+    "name": "Ullrich - Barrows",
+    "checked": true,
+    "users": [{
+        "name": "John",
+        "group": "Consumer",
+        "currentlyLoggedIn": false,
+        "country": "Germany"
+    }, {
+        "name": "Linda",
+        "group": "Contributor",
+        "currentlyLoggedIn": false,
+        "country": "United States"
+    }, {
+        "name": "Martin",
+        "group": "Developer",
+        "currentlyLoggedIn": false,
+        "country": "Italy"
+    }, {
+        "name": "Paul",
+        "group": "Admin",
+        "currentlyLoggedIn": false,
+        "country": "Italy"
+    }],
+    "generationUserId": "rZPZYbaWM33ZHNr6Z"
+};
+
+var customer2 = {
+    "_id": "EXpapRzZXc52B3joK",
+    "name": "Ullrich - Barrows",
+    "checked": true,
+    "users": [{
+        "name": "John",
+        "group": "Consumer",
+        "currentlyLoggedIn": false,
+        "country": "Germany"
+    }, {
+        "name": "Linda",
+        "group": "Contributor",
+        "currentlyLoggedIn": false,
+        "country": "United States"
+    }, {
+        "name": "Martin",
+        "group": "Developer",
+        "currentlyLoggedIn": false,
+        "country": "Italy"
+    }, {
+        "name": "Paul",
+        "group": "Admin",
+        "currentlyLoggedIn": false,
+        "country": "Italy"
+    }],
+    "generationUserId": "rZPZYbaWM33ZHNr6Z"
+};
