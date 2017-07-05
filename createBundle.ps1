9897eab9d76c289c78df46a83d5c9da0a9b85218
QHose <mbj@qlik.com>
2017-07-05 23:17:14 +0200

    enabled complete build script

--- createBundle.ps1
+++ createBundle.ps1
@@ -5,14 +5,14 @@ $DOCKER_TAG = "qhose/" + $BASE_APP_NAME + ":" + $VERSION
 $PORT = 80
 
 # echo "STEP delete old build files"
-# Remove-Item $BUILD_DIR\* -recurse -Force
+Remove-Item $BUILD_DIR\* -recurse -Force
 
 # echo "STEP build new meteor bundle"
-# meteor build --architecture=os.linux.x86_64 --allow-superuser --directory $BUILD_DIR
+meteor build --architecture=os.linux.x86_64 --allow-superuser --directory $BUILD_DIR
 
 echo "STEP copy dockerfile, settings.json and certificates to bundle folder"
 Copy-Item Dockerfile $BUILD_DIR\bundle
-Copy-Item *.pem $BUILD_DIR\bundle
+# Copy-Item *.pem $BUILD_DIR\bundle
 Copy-Item settings.json $BUILD_DIR\bundle
 Copy-Item run.sh $BUILD_DIR\bundle
 
