b42dbeff69d20d0720b7966bb9e7b13bff2c0357
QHose <mbj@qlik.com>
2017-07-10 17:00:14 +0200

    inserted the port back, inserted dns names for localhost

--- startNode.sh
+++ startNode.sh
@@ -1,2 +1,2 @@
-# export METEOR_SETTINGS=$(cat ./config/settings.json)
-node ./main.js -e METEOR_SETTINGS=$(cat ./config/settings.json)
+export METEOR_SETTINGS=$(cat ./config/settings.json)
+node ./main.js
