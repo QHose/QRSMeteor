7214f2d9c4eff55be5d21308f5ddb60ddcbaec6e
Martijn Biesbroek <mbj@qlik.com>
2017-09-13 22:26:18 +0200

    get slideheaders and slidedetails in useCaseSelection session

--- imports/ui/useCases/useCaseSelection.js
+++ imports/ui/useCases/useCaseSelection.js
@@ -9,6 +9,11 @@ import {
 } from '/imports/api/config.js';
 const enigma = require('enigma.js');
 const Cookies = require('js-cookie');
+var IntegrationPresentationSortedDataObject = Meteor.settings.public.slideGenerator.dataObject; //'pskL';//a table object in the saas presentation qvf, that ensures the slides are in the correct load order. better would be to load this in this order in the API call.
+var IntegrationPresentationSelectionSheet = Meteor.settings.public.slideGenerator.selectionSheet; //'DYTpxv'; selection sheet of the slide generator
+var slideObject = Meteor.settings.public.slideGenerator.dataObject;
+
+
 var possibleRoles = ['Developer', 'TECHNICAL', 'GENERIC', 'Product Owner', 'Hosting Ops', 'Business Analyst', 'CTO', 'C-Level, non-technical'];
 
 // ONRENDERED.
@@ -34,16 +39,117 @@ Template.useCaseSelection.onRendered(async function() {
     setTimeout(function() {
         $('.ui.dropdown')
             .dropdown({
-                async onChange(value, text, selItem) {
+                async onChange(group, text, selItem) {
                     Meteor.call('logoutPresentationUser', Meteor.userId(), Meteor.userId()); //udc and user are the same for presentation user                    
-                    Cookies.set('currentMainRole', value);
-                    // await getQlikSenseSessionForGroup('GENERIC');
-                    await getQlikSenseSessionForGroup(value);
-                    Router.go('slidegeneratorSlides');
+                    Cookies.set('currentMainRole', group);
+                    // await getQlikSenseSessionForGroup(value);
+                    await setSlideContentInSession(group);
+                    // Router.go('slidegeneratorSlides');
                 }
             })
     }, 1000)
 })
+
+async function setSlideContentInSession(group) {
+    try {
+        // get a valid ticket
+        var userProperties = {
+            group: group
+        };
+        var ticket = await Meteor.callPromise('getTicketNumber', userProperties);
+
+        const config = {
+            schema: senseConfig.QIXSchema,
+            appId: senseConfig.slideGeneratorAppId,
+            session: { //https://github.com/qlik-oss/enigma.js/blob/master/docs/qix/configuration.md#example-using-nodejs
+                host: senseConfig.host,
+                prefix: Meteor.settings.public.slideGenerator.virtualProxy,
+                port: senseConfig.port,
+                unsecure: true,
+                urlParams: {
+                    qlikTicket: ticket
+                }
+            },
+            listeners: {
+                'notification:*': (event, data) => console.log('Engima: event ' + event, 'Engima: data ' + data),
+            },
+            handleLog: (message) => console.log('Engima: ' + message),
+            //http://help.qlik.com/en-US/sense-developer/June2017/Subsystems/ProxyServiceAPI/Content/ProxyServiceAPI/ProxyServiceAPI-Msgs-Proxy-Clients-OnAuthenticationInformation.htm
+            // listeners: {
+            //     'notification:OnAuthenticationInformation': (authInfo) => {
+            //         // console.log('authInfo', authInfo)
+            //         if (authInfo.mustAuthenticate) {
+            //             location.href = authInfo.loginUri;
+            //         }
+            //     },
+            // }
+        };
+        var qix = await enigma.getService('qix', config);
+        console.log('Recieved qix object: ', qix)
+
+        //
+        // ─── MAIN TOPICS LEVEL 1 AND 2 ─────────────────────────────────────────────────
+        //
+
+
+        var model = await qix.app.getObject(IntegrationPresentationSortedDataObject) //get an existing object out of an app, if you import an app this stays the same
+        var data = await model.getHyperCubeData('/qHyperCubeDef', [{
+            qTop: 0,
+            qLeft: 0,
+            qWidth: 3,
+            qHeight: 1000
+        }]);
+        var table = data[0].qMatrix;
+        var tableWithChapters = insertSectionBreakers(table);
+        console.log('Received a table of data via the Engine API, now the slides can be created by impress.js', tableWithChapters);
+        Session.set('slideHeaders', tableWithChapters)
+        Meteor.setTimeout(function() {
+            if (Cookies.get('showSlideSorter') !== 'true') { //do not initialize impress so we can use the mobile device layout of impress to get all the slide under each other
+                // console.log('Show slideSorter NOT selected, so initialize impress.js');
+                impress().init();
+                impress().goto(0);
+            }
+            Session.set('slideLoading', false);
+        }, 2000);
+
+        //
+        // ─── GET LEVEL 1 TO 3 ────────────────────────────────────────────
+        //
+
+
+        var sessionModel = await qix.app.createSessionObject({
+            qInfo: {
+                qType: 'cube'
+            },
+            qHyperCubeDef: {
+                qDimensions: [{
+                    qDef: {
+                        qFieldDefs: ['Level 1']
+                    }
+                }, {
+                    qDef: {
+                        qFieldDefs: ['Level 2']
+                    }
+                }, {
+                    qDef: {
+                        qFieldDefs: ['Level 3']
+                    }
+                }]
+            }
+        });
+        sessionData = await sessionModel.getHyperCubeData('/qHyperCubeDef', [{
+            qTop: 0,
+            qLeft: 0,
+            qWidth: 3,
+            qHeight: 3333
+        }]);
+        Session.set('slideDetailsAllSlides', sessionData[0].qMatrix;);
+
+    } catch (error) {
+        console.error('ERROR getting level 1 and 2 from the app via the enigma.js: ', error);
+        sAlert.error('ERROR getting level 1 and 2 from the app via the enigma.js: ', error);
+    };
+}
 
 
 //ONDESTROYED
@@ -106,4 +212,26 @@ export async function getQlikSenseSessionForGroup(group) {
             console.info('info: No QIX connection for user, user not yet able to connect to the app via the enigma.js: ', error);
         });
 
+}
+
+
+function insertSectionBreakers(table) {
+    var currentLevel1, previousLevel1 = '';
+    var newTableWithChapter = [];
+
+    table.forEach(function(currentRow) {
+        var currentLevel1 = textOfLevel(currentRow, 1);
+        if (previousLevel1 !== currentLevel1) {
+            newTableWithChapter.push(currentLevel1)
+            previousLevel1 = currentLevel1;
+        }
+        newTableWithChapter.push(currentRow);
+    });
+    // console.log('table with chapters is', newTableWithChapter);
+    return newTableWithChapter;
+}
+
+function textOfLevel(row, level) {
+    level -= 1
+    return row[level].qText
 }
