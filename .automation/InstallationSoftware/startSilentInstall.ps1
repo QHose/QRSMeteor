a69632b164649ee6cb3be24178054a2dc5b5bde5
Martijn Biesbroek <mbj@qlik.com>
2017-10-08 14:16:44 +0200

    updated silent script

--- .automation/InstallationSoftware/startSilentInstall.ps1
+++ .automation/InstallationSoftware/startSilentInstall.ps1
@@ -1,6 +1,7 @@
-﻿$SHARED_FOLDER = "C:\QlikSenseShareTEdfdfdST"
+﻿$SHARED_NAME = "QlikSenseShare"
+$SHARED_FOLDER = "C:\"+$SHARED_NAME
 Write-Host 'Creating a shared folder on' $SHARED_FOLDER
 New-Item $SHARED_FOLDER –type directory
-New-SmbShare –Name 'QliKTESTSHARE' –Path $SHARED_FOLDER –FullAccess Everyone  
+New-SmbShare –Name $SHARED_NAME –Path $SHARED_FOLDER –FullAccess Everyone  
 
 # & 'c:\Qlik Sense Server.exe' -s -repair -l ".\log.txt" spc='.\spc.cfg' userwithdomain='QLIK-AB0Q2URN5T\Qlikexternal' userpassword='Database3'
