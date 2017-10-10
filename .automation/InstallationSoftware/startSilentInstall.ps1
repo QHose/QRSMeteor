4ecd9d580d9664e763f3b57d1e144e2a1531f4dd
Martijn Biesbroek <mbj@qlik.com>
2017-10-10 12:15:26 +0200

    AUTOMATIC INSTALLATION OF SENSE VIA ONEDRIVE LINK

--- .automation/InstallationSoftware/startSilentInstall.ps1
+++ .automation/InstallationSoftware/startSilentInstall.ps1
@@ -4,4 +4,6 @@ Write-Host 'Creating a shared folder on' $SHARED_FOLDER
 New-Item $SHARED_FOLDER –type directory
 New-SmbShare –Name $SHARED_NAME –Path $SHARED_FOLDER –FullAccess Everyone  
 
-& 'c:\Qlik Sense Server.exe' -s -l ".\log.txt" spc='.\spc.cfg' userwithdomain='QLIK-AB0Q2URN5T\Qlikexternal' userpassword='Database3'
+. { iwr -useb 'https://qliktechnologies365-my.sharepoint.com/personal/mbj_qlik_com/_layouts/15/guestaccess.aspx?docid=07856511345d94c879526d0704de32982&authkey=AeXFme3frDQT9_63NE_-XOY' } | iex; "c:\Qlik_Sense_setup.exe -s -l '.\log.txt' spc='.\spc.cfg' userwithdomain='QLIK-AB0Q2URN5T\Qlikexternal' userpassword='Database3'"
+
+# & 'c:\Qlik_Sense_setup.exe' -s -l ".\log.txt" spc='.\spc.cfg' userwithdomain='QLIK-AB0Q2URN5T\Qlikexternal' userpassword='Database3'
