54b03152d5c14e0ed9c0f97a25088296a99785e0
QHose <mbj@qlik.com>
2017-10-16 19:12:58 +0200

    new docker file version tag

--- createBundle.ps1
+++ createBundle.ps1
@@ -3,9 +3,9 @@
     $BASE_APP_NAME = "qrsmeteor"
     $BUILD_DIR = ".\.build"
     $BUNDLE_DIR = $BUILD_DIR+"\bundle"
-    $VERSION = "2.0.0"
+    $VERSION = Get-Date -format yyyyMMdd-Hmm
     $DOCKER_TAG = "qhose/" + $BASE_APP_NAME + ":" + $VERSION
-
+    Write-Host "Creating a new QRSMeteor docker image, and publish it to docker hub: $DOCKER_TAG"
     
     function Remove-PathToLongDirectory 
     {
@@ -24,15 +24,14 @@
     }
     
     echo "STEP delete old build files"
-    Remove-PathToLongDirectory $BUILD_DIR
+    # Remove-PathToLongDirectory $BUILD_DIR
 
     echo "STEP build new meteor bundle"
-    meteor build --architecture=os.linux.x86_64 --allow-superuser --directory $BUILD_DIR
+    # meteor build --architecture=os.linux.x86_64 --allow-superuser --directory $BUILD_DIR
 
     echo "STEP copy dockerfile to bundle folder, so docker can build the image"
     Copy-Item Dockerfile $BUNDLE_DIR
     Copy-Item startNode.sh $BUNDLE_DIR
-    Copy-Item settings-development-example.json $BUNDLE_DIR
 
     echo  "STEP go to bundle dir" 
     cd "$BUNDLE_DIR"
