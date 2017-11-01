101bf5a7883a37161b63a8918875ed9dbf833e90
QHose <mbj@qlik.com>
2017-11-01 18:51:48 +0100

    update automation files

--- .automation/InstallationSoftware/install software step 2 - clone projects.bat
+++ .automation/InstallationSoftware/install software step 2 - clone projects.bat
@@ -1,3 +1,12 @@
 echo clone github projects
 git clone https://github.com/QHose/QRSMeteor.git --branch simplify-settings-file c:\github\QRSMeteor
 git clone https://github.com/QHose/senseWebIntegration.git c:\github\SenseWebIntegration
+
+echo install qrsmeteor npm modules
+cd c:\github\QRSMeteor
+meteor npm install --save
+
+
+echo install sensewebintegration npm modules
+cd c:\github\SenseWebIntegration
+meteor npm install --save
--- install modules.bat
+++
@@ -1,1 +0,0 @@
-meteor npm install --save
