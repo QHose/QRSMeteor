e13157123db4351e35b6fdacd598ba9ba61a4806
QHose <mbj@qlik.com>
2017-11-01 18:25:14 +0100

    automatic installation

---
+++ .automation/InstallationSoftware/install software step 2 - clone projects.bat
@@ -0,0 +1,3 @@
+echo clone github projects
+git clone https://github.com/QHose/QRSMeteor.git --branch simplify-settings-file c:\github\QRSMeteor
+git clone https://github.com/QHose/senseWebIntegration.git c:\github\SenseWebIntegration
--- .automation/InstallationSoftware/startSilentInstall.ps1
+++ .automation/InstallationSoftware/startSilentInstall.ps1
@@ -4,14 +4,6 @@ Write-Host 'Creating a shared folder on' $SHARED_FOLDER
 New-Item $SHARED_FOLDER –type directory
 New-SmbShare –Name $SHARED_NAME –Path $SHARED_FOLDER –FullAccess Everyone  
 
-
-# Write-Host 'Start Meteor installer. You do not need to register, just press "skip"'
-# Start-Process -FilePath '.\InstallMeteor.exe'
-
-# Write-Host 'Start installation of Github, with this version management tool we keep QRSMeteor up to date via the "sync button". You do not need to register an account. Press skip in the bottom of the screen.'
-# Start-Process -FilePath '.\GitHubDesktopSetup.exe'
-
-
 Write-Host 'Download and start Qlik Sense installer...'
 # . { iwr -useb 'https://qliktechnologies365-my.sharepoint.com/personal/mbj_qlik_com/_layouts/15/guestaccess.aspx?docid=07856511345d94c879526d0704de32982&authkey=AeXFme3frDQT9_63NE_-XOY' -OutFile 'C:\Qlik_Sense_setup.exe' } | 
 
