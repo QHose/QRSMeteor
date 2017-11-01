cba8445adf0eef13651b3919367aabd3b96f611c
QHose <mbj@qlik.com>
2017-11-01 19:40:05 +0100

    added try catch around upload app

--- .automation/InstallationSoftware/install software step 2 - clone projects.bat
+++ .automation/InstallationSoftware/install software step 2 - clone projects.bat
@@ -1,12 +1,12 @@
-echo clone github projects
-git clone https://github.com/QHose/QRSMeteor.git --branch simplify-settings-file c:\github\QRSMeteor
-git clone https://github.com/QHose/senseWebIntegration.git c:\github\SenseWebIntegration
+echo clone GitHub projects
+git clone https://GitHub.com/QHose/QRSMeteor.git --branch simplify-settings-file c:\GitHub\QRSMeteor
+git clone https://GitHub.com/QHose/senseWebIntegration.git c:\GitHub\SenseWebIntegration
 
 echo install qrsmeteor npm modules
-cd c:\github\QRSMeteor
+cd c:\GitHub\QRSMeteor
 meteor npm install --save
 
 
 echo install sensewebintegration npm modules
-cd c:\github\SenseWebIntegration
+cd c:\GitHub\SenseWebIntegration
 meteor npm install --save
--- imports/api/server/QRSFunctionsApp.js
+++ imports/api/server/QRSFunctionsApp.js
@@ -414,30 +414,35 @@ function checkTemplateAppExists(generationUserId) {
 
 
 async function uploadApp(filePath, appName) {
-    console.log('Upload app: ' + appName + ' from path: ' + filePath);
+    console.log('Upload app: ' + appName + ' from path: ' + filePath + ' via header authentication server: ' + qlikHDRServer);
     return await new Promise(function(resolve, reject) {
         var formData = {
             my_file: fs.createReadStream(filePath)
         };
 
-        request.post({
-            url: qlikHDRServer + '/qrs/app/upload?name=' + appName + '&xrfkey=' + senseConfig.xrfkey,
-            headers: {
-                'Content-Type': 'application/vnd.qlik.sense.app',
-                'hdr-usr': senseConfig.headerValue,
-                'X-Qlik-xrfkey': senseConfig.xrfkey
-            },
-            formData: formData
-        }, function(error, res, body) {
-            if (!error) {
-                var appId = JSON.parse(body).id;
-                console.log('Uploaded "' + appName + '.qvf" to Qlik Sense and got appID: ' + appId);
-                resolve(appId);
-            } else {
-                console.error("Failed to upload app" + appName, error);
-                reject(error);
-            }
-        });
+        try {
+            request.post({
+                url: qlikHDRServer + '/qrs/app/upload?name=' + appName + '&xrfkey=' + senseConfig.xrfkey,
+                headers: {
+                    'Content-Type': 'application/vnd.qlik.sense.app',
+                    'hdr-usr': senseConfig.headerValue,
+                    'X-Qlik-xrfkey': senseConfig.xrfkey
+                },
+                formData: formData
+            }, function(error, res, body) {
+                if (!error) {
+                    var appId = JSON.parse(body).id;
+                    console.log('Uploaded "' + appName + '.qvf" to Qlik Sense and got appID: ' + appId);
+                    resolve(appId);
+                } else {
+                    console.error("Failed to upload app" + appName, error);
+                    reject(error);
+                }
+            });
+        } catch (error) {
+            console.error('failed to upload app', error);
+        }
+
     });
 }
 //
@@ -524,7 +529,6 @@ function checkStreamStatus(customer, generationUserId) {
 
 export function getApps(name, stream) {
     var path = '/qrs/app/full';
-    console.log('getApps(name, stream ' + name + ' ' + stream + ' via API path: ' + path);
 
     //if a name/stream is provided only search the apps with this name
     if (name) {
@@ -533,6 +537,7 @@ export function getApps(name, stream) {
             path += " and stream.name eq '" + stream + "'"
         }
     }
+    console.log('getApps(name: ' + name + ' and stream ' + stream + 'via API path: ' + path);
 
     var call = {
         action: 'Get list of apps',
