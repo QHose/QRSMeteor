cfd986d6e4d9d7897428fcfaddfe0de093d314db
QHose <mbj@qlik.com>
2017-11-08 18:22:47 +0100

    add call statements to bat files

--- public/docs/install software step 1.bat
+++ public/docs/install software step 1.bat
@@ -2,32 +2,26 @@ Echo install choco
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 Echo install software
-choco install github-desktop git -params '"/GitAndUnixToolsOnPath"' -y
-choco install meteor 7zip.install firefox googlechrome visualstudiocode adobereader openoffice notepadplusplus qdir logfusion dotnet4.5.2 lastpass microsoft-build-tools vcbuildtools -y
+choco install meteor github-desktop git -params '"/GitAndUnixToolsOnPath"' dotnet4.5.2 -y
+REM choco install firefox googlechrome visualstudiocode logfusion microsoft-build-tools adobereader openoffice qdir notepadplusplus lastpass 7zip.install vcbuildtools -y
 
-choco install nodejs --version 8.8.1 -y
+REM choco install nodejs --version 8.8.1 -y
 
 c:\windows\syswow64\WindowsPowerShell\v1.0\powershell.exe -command set-executionpolicy remotesigned
 
 echo refresh environment variables and paths
-refreshenv
+call refreshenv
 
 echo clone GitHub projects
 git clone https://GitHub.com/QHose/QRSMeteor.git --branch simplify-settings-file c:\GitHub\QRSMeteor
 git clone https://GitHub.com/QHose/senseWebIntegration.git c:\GitHub\SenseWebIntegration
 
-echo install module to read json files via node (to be able to set env. vars.)
-npm install -g json
-
-echo install windows build tools
-npm install --global windows-build-tools
-
 echo install qrsmeteor npm modules
 cd c:\GitHub\QRSMeteor
-meteor npm install --save
+call meteor npm install --save
 
 
 echo install sensewebintegration npm modules
 cd c:\GitHub\SenseWebIntegration
-meteor npm install --save
+call meteor npm install --save
 
