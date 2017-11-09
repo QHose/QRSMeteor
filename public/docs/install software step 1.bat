c19acff9a88307325a1330e1b61d293ecf6abdb2
Martijn Biesbroek <mbj@qlik.com>
2017-11-09 18:47:24 +0100

    update bat

--- public/docs/install software step 1.bat
+++ public/docs/install software step 1.bat
@@ -2,9 +2,9 @@ Echo install choco
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 Echo install software
-choco install meteor github-desktop git -params '"/GitAndUnixToolsOnPath"' dotnet4.5.2 visualstudiocode logfusion microsoft-build-tools vcbuildtools -y
-REM choco install firefox googlechrome visualstudiocode logfusion microsoft-build-tools adobereader openoffice qdir notepadplusplus lastpass 7zip.install vcbuildtools -y
-
+choco install meteor github-desktop googlechrome git -params '"/GitAndUnixToolsOnPath"' dotnet4.5.2 visualstudiocode logfusion microsoft-build-tools vcbuildtools -y
+REM choco install firefox adobereader openoffice qdir notepadplusplus lastpass 7zip.install -y
+REM choco install vlc skype adobereader googlechrome avgantivirusfree 7zip.install dropbox lastpass onedrive -y
 REM choco install nodejs --version 8.8.1 -y
 
 c:\windows\syswow64\WindowsPowerShell\v1.0\powershell.exe -command set-executionpolicy remotesigned
