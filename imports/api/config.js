e2b9fffbe691c9b83fba277b22f060401b1b28c4
Martijn Biesbroek <mbj@qlik.com>
2019-04-01 10:21:57 +0200

    automatic port selection based on https

--- imports/api/config.js
+++ imports/api/config.js
@@ -11,7 +11,7 @@ const _QIXSchema = require('/node_modules/enigma.js/schemas/qix/12.20.0/schema.j
 if (Meteor.isClient) {
     var _senseConfig = {
         "host": Meteor.settings.public.qlikSenseHost,
-        "port": Meteor.settings.public.qlikSensePort,
+        "port": Meteor.settings.public.useSSL? Meteor.settings.public.qlikSensePortSecure:Meteor.settings.public.qlikSensePort,
         "virtualProxyClientUsage": Meteor.settings.public.virtualProxyClientUsage,
         "virtualProxySlideGenerator": Meteor.settings.public.slideGenerator.virtualProxy,
         "webIntegrationDemoPort": Meteor.settings.public.webIntegrationDemoPort,
