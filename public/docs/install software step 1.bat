7b03ef679dfb4bbd3b782ccc3293020f11132d1c
Martijn Biesbroek <mbj@qlik.com>
2020-07-27 11:59:48 +0200

    Update install software step 1.bat

--- public/docs/install software step 1.bat
+++ public/docs/install software step 1.bat
@@ -1,5 +1,5 @@
 Echo install choco
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command " [System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 Echo install software
 choco install meteor github-desktop googlechrome git -params '"/GitAndUnixToolsOnPath"' dotnet4.5.2 visualstudiocode logfusion microsoft-build-tools vcbuildtools -y
