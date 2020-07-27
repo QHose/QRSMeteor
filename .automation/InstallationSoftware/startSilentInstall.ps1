de52fac7a35f46c6d3c50940cb11fb7f4e0b5fc3
Martijn Biesbroek <mbj@qlik.com>
2020-07-27 14:37:09 +0200

    Update startSilentInstall.ps1

--- .automation/InstallationSoftware/startSilentInstall.ps1
+++ .automation/InstallationSoftware/startSilentInstall.ps1
@@ -29,7 +29,7 @@ else {
 Write-Host "Ensuring password for $Username never expires."
 & WMIC USERACCOUNT WHERE "Name='$Username'" SET PasswordExpires=FALSE
 
-$url = "http://gofile.me/6mp1l/iw93s03Mr"
+$url = "https://www.dropbox.com/s/4u0c3hf6vf3d80b/Qlik_Sense_setup.exe?dl=0"
 $QlikSenseInstaller = "$SHARED_FOLDER\Qlik_Sense_setup.exe"
 $start_time = Get-Date
 
