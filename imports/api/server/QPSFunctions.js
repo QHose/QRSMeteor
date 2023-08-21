6862b7d4c13e67068a8b97c533be50bd0854aa16
Martijn Biesbroek <bieshose@gmail.com>
2023-08-21 20:10:23 +0200

    1.9 update gyp issue

--- imports/api/server/QPSFunctions.js
+++ imports/api/server/QPSFunctions.js
@@ -11,7 +11,7 @@ const path = require("path");
 var os = require("os");
 var ip = require("ip");
 // const token = require("./token");
-const { v4: uuidv4 } = require("uuid");
+// const { v4: uuidv4 } = require("uuid");
 
 
 //
@@ -407,25 +407,25 @@ Meteor.methods({
   },
   //https://qlik.dev/authenticate/jwt/create-signed-tokens-for-jwt-authorization
   getJWTToken(passport) {
-    console.log("get JWT with passport", passport);
+    console.log("get JWT with passport DISABLED, YOU SHOULD NOT SEE THIS!!!!!", passport);
 
-    try {
-      const uuid = uuidv4();
-      const sub = `sub_${uuid}`;
-      const name = passport.UserId;
-      const email = `${uuid}@demo.anon`;
-      const groups = passport.Groups;
+    // try {
+    //   const uuid = uuidv4();
+    //   const sub = `sub_${uuid}`;
+    //   const name = passport.UserId;
+    //   const email = `${uuid}@demo.anon`;
+    //   const groups = passport.Groups;
 
-      const genT = token.generate(sub, name, email, groups);
-      console.log("🚀 ~ file: QPSFunctions.js:423 ~ getJWTToken ~ genT:", genT)
-      return genT;
-    } catch (err) {
-      console.error(
-        "unable to generate JWT token, did you supply the correct public.pem and private.pem in the dir: "+Meteor.settings.private.certificatesDirectory,
-        err
-      );
-      throw new Meteor.Error("generate jwt token failed", err.message);
-    }
+    //   const genT = token.generate(sub, name, email, groups);
+    //   console.log("🚀 ~ file: QPSFunctions.js:423 ~ getJWTToken ~ genT:", genT)
+    //   return genT;
+    // } catch (err) {
+    //   console.error(
+    //     "unable to generate JWT token, did you supply the correct public.pem and private.pem in the dir: "+Meteor.settings.private.certificatesDirectory,
+    //     err
+    //   );
+    //   throw new Meteor.Error("generate jwt token failed", err.message);
+    // }
   },
   resetLoggedInUser() {
     // console.log("***Method resetLoggedInUsers");
